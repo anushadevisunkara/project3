cf12aaaeeda771a7fb4ad3defc4dc819d4d0ba10
anusha <anushadevi.sunkara@gmail.com>
2018-04-17 21:51:02 +0530

    initial commit

---
+++ src/main/java/project3/pro3.java
@@ -0,0 +1,8 @@
+package project3;
+
+public class pro3 {
+	public String sample() {
+		return "Sample Demo Java Class";
+	}
+}
+
---
+++ src/test/java/project3/pro3.java
@@ -0,0 +1,12 @@
+package project3;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class pro3 {
+
+	@Test
+	public void test() {
+		}
+}
